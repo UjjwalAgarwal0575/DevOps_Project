47fe459f3cad00183ecc4fbc9599dc89ebd401e7
UjjwalAgarwal0575 <agarwal.ujjwal02@gmail.com>
2023-11-26 13:46:33 +0530

    Updated run.sh

--- backend/src/main/java/com/example/springbootbackend/Test/run.sh
+++ backend/src/main/java/com/example/springbootbackend/Test/run.sh
@@ -1,6 +1,7 @@
-initMem=${7500}
-g++ -o solution 1.cpp &> $1
-#  && {
+# initMem=${7500}
+# g++ -o solution solution.cpp &> $1 
+
+# && {
             # {
             #     cat testcase.txt | /usr/bin/time -f "%e %M" -o $3 timeout $4s ./solution &> $2
             #     # cat testcase.txt | ts=$(date +%s%N) -o $3 timeout $4s ./solution tt=$((($(date +%s%N) - $ts)/1000000)) ; echo "Time taken: $tt milliseconds"
@@ -8,3 +9,9 @@ g++ -o solution 1.cpp &> $1
             #     RTE=1
             # }
             # }
+
+g++ -o solution $1 &> $2 && {
+    {
+        cat $3 | ./solution &> $2
+    } 
+}
