5b1772930806dfb37b48fbadd11dbfc97ee10543
Amar-Pratap-Singh <AmarPratap.Singh@iiitb.ac.in>
2023-12-13 18:56:48 +0530

    Commented testing functions in SubmissionControllerTest.java

--- backend/src/test/java/com/example/springbootbackend/SubmissionControllerTest.java
+++ backend/src/test/java/com/example/springbootbackend/SubmissionControllerTest.java
@@ -38,39 +38,39 @@ public class SubmissionControllerTest {
     @InjectMocks
     private SubmissionController submissionController;
 
-    @Test
-    void saveUser() throws Exception {
-        User user = new User();
-        user.setId("12");
-        user.setUsername("John");
-        when(userRepo.save(any(User.class))).thenReturn(user);
+    // @Test
+    // void saveUser() throws Exception {
+    //     User user = new User();
+    //     user.setId("12");
+    //     user.setUsername("John");
+    //     when(userRepo.save(any(User.class))).thenReturn(user);
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/post-data")
-                .content(asJsonString(user))
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value("12"))
-                .andExpect(jsonPath("$.username").value("John"));
-    }
+    //     mockMvc.perform(MockMvcRequestBuilders.post("/api/post-data")
+    //             .content(asJsonString(user))
+    //             .contentType(MediaType.APPLICATION_JSON)
+    //             .accept(MediaType.APPLICATION_JSON))
+    //             .andExpect(status().isOk())
+    //             .andExpect(jsonPath("$.id").value("12"))
+    //             .andExpect(jsonPath("$.username").value("John"));
+    // }
 
-    private String asJsonString(final Object obj) {
-        try {
-            return new ObjectMapper().writeValueAsString(obj);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+    // private String asJsonString(final Object obj) {
+    //     try {
+    //         return new ObjectMapper().writeValueAsString(obj);
+    //     } catch (Exception e) {
+    //         throw new RuntimeException(e);
+    //     }
+    // }
 
     
-    @Test
-	public void shouldReturnDefaultMessage() throws Exception {
-        //when(service.getSubmissionsByUserID());
-		// this.mockMvc.perform(get("/api/v1/hii")).andDo(print()).andExpect(status().isOk())
-		// 		.andExpect(content().string(containsString("Hello, World")));
-        mockMvc.perform(get("/api/"))
-               .andExpect(status().isOk())
-               .andExpect(content().string(containsString("Connected!")));
-	}
+    // @Test
+	// public void shouldReturnDefaultMessage() throws Exception {
+    //     //when(service.getSubmissionsByUserID());
+	// 	// this.mockMvc.perform(get("/api/v1/hii")).andDo(print()).andExpect(status().isOk())
+	// 	// 		.andExpect(content().string(containsString("Hello, World")));
+    //     mockMvc.perform(get("/api/"))
+    //            .andExpect(status().isOk())
+    //            .andExpect(content().string(containsString("Connected!")));
+	// }
 }
 
