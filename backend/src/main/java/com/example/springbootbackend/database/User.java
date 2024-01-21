e3b0da91b969d65cc3b8ba6be6f4e1aa391317f9
UjjwalAgarwal0575 <agarwal.ujjwal02@gmail.com>
2024-01-21 20:56:06 +0530

    Added APIs for authentication

--- backend/src/main/java/com/example/springbootbackend/database/User.java
+++ backend/src/main/java/com/example/springbootbackend/database/User.java
@@ -7,6 +7,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+// import jakarta.validation.constraints.NotBlank;
+// import jakarta.validation.constraints.Size;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,24 +18,55 @@ public class User {
     
     @Id
     private String id;
-    private String username;
+  
+    // @NotBlank
+    // @Size(max = 20)
+    private String username;    
+  
+    // @NotBlank
+    // @Size(max = 50)
+    private String emailId;
 
-
-    public String getId(){
-        return this.id;
+    // @NotBlank
+    // @Size(max = 120)
+    private String password;
+  
+    public User(String username,String emailId, String password) {
+      this.username = username;
+      this.emailId = emailId;
+      this.password = password;
+    }
+  
+    public String getId() {
+      return id;
+    }
+  
+    public void setId(String id) {
+      this.id = id;
+    }
+  
+    public String getUsername() {
+      return username;
+    }
+  
+    public void setUsername(String username) {
+      this.username = username;
+    }
+  
+    public String getEmailId() {
+        return emailId;
+      }
+    
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
     }
 
-    public String getUsername(){
-        return this.username;
+    public String getPassword() {
+      return password;
     }
-
-    public void setId(String id){
-        this.id = id;
+  
+    public void setPassword(String password) {
+      this.password = password;
     }
-
-    public void setUsername(String username){
-        this.username = username;
-    }
-
 
 }
