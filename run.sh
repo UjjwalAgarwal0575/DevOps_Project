228943fdd8a3cf8d0b46f13a745afe06b430c792
introverted_monke_69 <77499684+Bimg-Brein42069@users.noreply.github.com>
2024-04-14 12:08:55 +0530

    changed run script

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+docker rm -vf $(docker ps -aqf name=worker)
+docker rm -vf $(docker ps -aqf name=sender)
+docker rm -vf $(docker ps -aqf name=frontend)
+docker rmi -f worker
+docker rmi -f sender
+docker rmi -f frontend
+
+cd sender
+mvn clean install
+docker build --tag 'sender' .
+cd ..
+
+cd worker
+mvn clean install
+docker build --tag 'worker' .
+cd ..
+
+cd frontend
+npm install
+docker build --tag 'frontend' .
+cd ..
+
+docker-compose up
